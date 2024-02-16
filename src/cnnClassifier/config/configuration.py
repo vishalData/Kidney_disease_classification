f9f6e74a6cb3e73f5fbca6bb0fff106ac8a85278
vishalData <vishalverma272000@gmail.com>
2024-02-16 16:23:19 +0530

    model training

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -55,7 +55,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-images")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney_ct_images")
         create_directories([
             Path(training.root_dir)
         ])
